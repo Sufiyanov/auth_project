cf0812bac1d528febb59010e768df63e91acf348
Rushan <rrsufiyanov@gmail.com>
2020-12-13 13:12:27 +0300

    add token auth

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const analyticsRoutes  = require('./routes/analytics')
 const authRoutes  = require('./routes/auth')
 const categoryRoutes  = require('./routes/category')
@@ -9,9 +10,12 @@ const positionRoutes  = require('./routes/position')
 const keys = require('./config/keys')
 const app = express();
 
-mongoose.connect(keys.mongoURI)
+mongoose.connect(keys.mongoURI, {useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => console.log('MongoDB connected.'))
     .catch(error => console.log(error))
+
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
 
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcryptjs')
 const jsonwebtoken = require('jsonwebtoken')
 const User = require('../models/User')
-const keys = require('../config/keys')
+const keys = require('../config/keys');
+const errorHandler = require('../utils/errorHandler');
 
 
 module.exports.login =  async function(req, res) {
@@ -47,7 +48,7 @@ module.exports.register = async function(req, res) {
          await user.save()
          res.status(201).json(user)
         } catch (e){
-
+            errorHandler(res, e)
         }
         }
 };
